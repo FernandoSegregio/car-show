2605a4186449a0787bd83487ee0d80275752fae6
Fernando Segregio <segregio@gmail.com>
2022-05-30 23:30:53 -0300

    style: set brand color

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,6 +2,14 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 
+:root{
+    /* brand-colors */
+  --neutral-color-white: #EBEFF2;
+  --brand-color-tertiary: #D2D4D9;
+  --brand-color-secundary: #AFAFAF;
+  --brand-color-primary: #1D2527;
+}
+
   * {
     margin: 0;
     padding: 0;
