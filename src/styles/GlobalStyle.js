3b2aa5217b7912fd7a29257d0d4ad7e92182c3ff
Fernando Segregio <segregio@gmail.com>
2022-05-31 00:28:32 -0300

    style: add line height project

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -12,6 +12,12 @@ const GlobalStyle = createGlobalStyle`
   --font-size-m : 1.25rem /* 20px */
   --font-size-l: 1.5rem; /* 24px */ 
   --font-size-xl: 2rem; /* 32px */
+  --font-size-xxl: 3.5rem; /* 56px */
+
+    /* line height */
+  --line-height-default: 100%;
+  --line-height-m: 150%;
+  --line-height-l: 200%;
     /* brand-colors */
   --neutral-color-white: #EBEFF2;
   --brand-color-tertiary: #D2D4D9;
