751dac4f37ac2273100082e2086236c527c606df
Fernando Segregio <segregio@gmail.com>
2022-06-01 12:42:21 -0300

    feat: add components Main and title

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MainContainer, SectionMain } from './style';
+
+function Main() {
+  return (
+    <MainContainer>
+      <SectionMain>
+        <h1>MUSTANG</h1>
+      </SectionMain>
+    </MainContainer>
+  );
+}
+
+export default Main;
