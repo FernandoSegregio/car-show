cba3067a9f9adc2b8ed365f403509ee316e440ba
Fernando Segregio <segregio@gmail.com>
2022-06-01 00:18:05 -0300

    feat: add menu hamburguer, and create logical for open

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import HeaderContainer from './style';
+import React, { useState } from 'react';
+import {
+  HeaderContainer,
+  NavHeader,
+  Hamburguer,
+} from './style';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  function openMenu() {
+    return isOpen ? setIsOpen(false) : setIsOpen(true);
+  }
+
   return (
-    <HeaderContainer>Header</HeaderContainer>
+    <HeaderContainer>
+      <Hamburguer onClick={() => openMenu()} type="button" hidden={!isOpen}>
+        <span className="iconify" data-icon="ci:hamburger" />
+      </Hamburguer>
+      <NavHeader hidden={isOpen}>
+        Header
+      </NavHeader>
+    </HeaderContainer>
   );
 }
 
