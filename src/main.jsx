23c829b543cca05b591ad2732ea82914d53b0a05
Fernando Segregio <segregio@gmail.com>
2022-06-01 01:19:34 -0300

    style: add GlobalStyle

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import CarShowProvider from './context/CarShowProvider';
+import GlobalStyle from './styles/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CarShowProvider>
+      <GlobalStyle />
       <App />
     </CarShowProvider>
   </React.StrictMode>,
