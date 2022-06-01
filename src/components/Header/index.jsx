c3bffea032bed708ce7f1947aa7815d9976fee2a
Fernando Segregio <segregio@gmail.com>
2022-05-31 23:28:08 -0300

    feat: create component HeaderContainer and render in HomePage

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeaderContainer from './style';
+
+function Header() {
+  return (
+    <HeaderContainer>Header</HeaderContainer>
+  );
+}
+
+export default Header;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function HomePage() {
   return (
-    <div>ShowCar</div>
+    <Header />
   );
 }
 
