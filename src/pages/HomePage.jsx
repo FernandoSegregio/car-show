78869930ae224366868241392464a0ad175f5b4e
Fernando Segregio <segregio@gmail.com>
2022-06-01 12:42:41 -0300

    feat: render main in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Main from '../components/Main';
 
 function HomePage() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Main />
+    </>
   );
 }
 
