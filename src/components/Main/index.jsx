acbdb47cebb23470535e7ece43e5b2a30a0488d5
Fernando Segregio <segregio@gmail.com>
2022-06-02 03:28:25 -0300

    feat: add div carousel static

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import {
   ArticleAbout,
   ButtonAbout,
+  ButtonCarousel,
+  CarouselContainer,
+  DivCarousel,
   MainContainer, SectionAbout, SectionMain,
 } from './style';
+
+import car1 from '../../assets/images/cars/car1.svg';
 
 function Main() {
   return (
@@ -11,7 +16,31 @@ function Main() {
       <SectionMain>
         <h1>MUSTANG</h1>
       </SectionMain>
-      <div>carrousel</div>
+      <CarouselContainer>
+        <DivCarousel>
+          <img src={car1} alt="car honda aversa" />
+          <h2>Honda Aversa</h2>
+          <ul>
+            <li>
+              <span className="iconify date" data-icon="bi:calendar2-date" />
+              <span>2015</span>
+            </li>
+            <li>
+              <span className="iconify velo" data-icon="fluent:top-speed-20-regular" />
+              <span>180 km/h</span>
+            </li>
+            <li>
+              <span className="iconify energy" data-icon="simple-line-icons:energy" />
+              <span>9/10</span>
+            </li>
+            <li>
+              <span className="iconify people" data-icon="fluent:people-20-regular" />
+              <span>8/10</span>
+            </li>
+          </ul>
+          <ButtonCarousel>Ver carro</ButtonCarousel>
+        </DivCarousel>
+      </CarouselContainer>
       <SectionAbout>
         <div />
         <ArticleAbout>
