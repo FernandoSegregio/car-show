9b523c0e4c936f758272ceb4991f712ec2d99ab6
Fernando Segregio <segregio@gmail.com>
2022-06-02 13:10:31 -0300

    feat: change carousel dinamic

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,23 +1,18 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
-
 import { SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/bundle';
 
-// import required modules
 import { Pagination, Navigation } from 'swiper';
-
 import {
   ButtonCarousel,
   CarouselContainer,
 } from './style';
-
-import car1 from '../../assets/images/cars/car1.svg';
+import data from '../../helper/database';
 
 function Carousel() {
   return (
@@ -34,144 +29,35 @@ function Carousel() {
       modules={[Pagination, Navigation]}
       className="mySwiper"
     >
-      <SwiperSlide>
-        <img src={car1} alt="car honda aversa" />
-        <h2>Honda Aversa</h2>
-        <ul>
-          <li>
-            <span className="iconify date" data-icon="bi:calendar2-date" />
-            <span>2015</span>
-          </li>
-          <li>
-            <span className="iconify velo" data-icon="fluent:top-speed-20-regular" />
-            <span>180 km/h</span>
-          </li>
-          <li>
-            <span className="iconify energy" data-icon="simple-line-icons:energy" />
-            <span>9/10</span>
-          </li>
-          <li>
-            <span className="iconify people" data-icon="fluent:people-20-regular" />
-            <span>8/10</span>
-          </li>
-        </ul>
-        <ButtonCarousel>Ver carro</ButtonCarousel>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={car1} alt="car honda aversa" />
-        <h2>Honda Aversa</h2>
-        <ul>
-          <li>
-            <span className="iconify date" data-icon="bi:calendar2-date" />
-            <span>2015</span>
-          </li>
-          <li>
-            <span className="iconify velo" data-icon="fluent:top-speed-20-regular" />
-            <span>180 km/h</span>
-          </li>
-          <li>
-            <span className="iconify energy" data-icon="simple-line-icons:energy" />
-            <span>9/10</span>
-          </li>
-          <li>
-            <span className="iconify people" data-icon="fluent:people-20-regular" />
-            <span>8/10</span>
-          </li>
-        </ul>
-        <ButtonCarousel>Ver carro</ButtonCarousel>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={car1} alt="car honda aversa" />
-        <h2>Honda Aversa</h2>
-        <ul>
-          <li>
-            <span className="iconify date" data-icon="bi:calendar2-date" />
-            <span>2015</span>
-          </li>
-          <li>
-            <span className="iconify velo" data-icon="fluent:top-speed-20-regular" />
-            <span>180 km/h</span>
-          </li>
-          <li>
-            <span className="iconify energy" data-icon="simple-line-icons:energy" />
-            <span>9/10</span>
-          </li>
-          <li>
-            <span className="iconify people" data-icon="fluent:people-20-regular" />
-            <span>8/10</span>
-          </li>
-        </ul>
-        <ButtonCarousel>Ver carro</ButtonCarousel>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={car1} alt="car honda aversa" />
-        <h2>Honda Aversa</h2>
-        <ul>
-          <li>
-            <span className="iconify date" data-icon="bi:calendar2-date" />
-            <span>2015</span>
-          </li>
-          <li>
-            <span className="iconify velo" data-icon="fluent:top-speed-20-regular" />
-            <span>180 km/h</span>
-          </li>
-          <li>
-            <span className="iconify energy" data-icon="simple-line-icons:energy" />
-            <span>9/10</span>
-          </li>
-          <li>
-            <span className="iconify people" data-icon="fluent:people-20-regular" />
-            <span>8/10</span>
-          </li>
-        </ul>
-        <ButtonCarousel>Ver carro</ButtonCarousel>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={car1} alt="car honda aversa" />
-        <h2>Honda Aversa</h2>
-        <ul>
-          <li>
-            <span className="iconify date" data-icon="bi:calendar2-date" />
-            <span>2015</span>
-          </li>
-          <li>
-            <span className="iconify velo" data-icon="fluent:top-speed-20-regular" />
-            <span>180 km/h</span>
-          </li>
-          <li>
-            <span className="iconify energy" data-icon="simple-line-icons:energy" />
-            <span>9/10</span>
-          </li>
-          <li>
-            <span className="iconify people" data-icon="fluent:people-20-regular" />
-            <span>8/10</span>
-          </li>
-        </ul>
-        <ButtonCarousel>Ver carro</ButtonCarousel>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={car1} alt="car honda aversa" />
-        <h2>Honda Aversa</h2>
-        <ul>
-          <li>
-            <span className="iconify date" data-icon="bi:calendar2-date" />
-            <span>2015</span>
-          </li>
-          <li>
-            <span className="iconify velo" data-icon="fluent:top-speed-20-regular" />
-            <span>180 km/h</span>
-          </li>
-          <li>
-            <span className="iconify energy" data-icon="simple-line-icons:energy" />
-            <span>9/10</span>
-          </li>
-          <li>
-            <span className="iconify people" data-icon="fluent:people-20-regular" />
-            <span>8/10</span>
-          </li>
-        </ul>
-        <ButtonCarousel>Ver carro</ButtonCarousel>
-      </SwiperSlide>
+      {
+      data.map(({
+        id, image, alt, name, year, speed, energyRating, userRating, model,
+      }) => (
+        <SwiperSlide key={id}>
+          <img src={image} alt={alt} className={model} />
+          <h2>{name}</h2>
+          <ul>
+            <li>
+              <span className="iconify date" data-icon="bi:calendar2-date" />
+              <span>{year}</span>
+            </li>
+            <li>
+              <span className="iconify velo" data-icon="fluent:top-speed-20-regular" />
+              <span>{`${speed} Km/h`}</span>
+            </li>
+            <li>
+              <span className="iconify energy" data-icon="simple-line-icons:energy" />
+              <span>{`${energyRating}/10`}</span>
+            </li>
+            <li>
+              <span className="iconify people" data-icon="fluent:people-20-regular" />
+              <span>{`${userRating}/10`}</span>
+            </li>
+          </ul>
+          <ButtonCarousel>Ver carro</ButtonCarousel>
+        </SwiperSlide>
+      ))
+      }
     </CarouselContainer>
   );
 }
