14edf3e2b84fc4206f481b5b5c214f0946040cbf
Fernando Segregio <segregio@gmail.com>
2022-06-02 13:08:50 -0300

    chore: add database cars

---
+++ src/helper/database.js
@@ -0,0 +1,41 @@
+import car1 from '../assets/images/cars/car1.svg';
+import car2 from '../assets/images/cars/car2.svg';
+import car3 from '../assets/images/cars/car3.svg';
+
+const data = [
+  {
+    id: 1,
+    image: car1,
+    alt: 'car mustang',
+    name: 'Mustang',
+    year: 2015,
+    speed: 180,
+    energyRating: 9,
+    userRating: 8,
+    model: 'car1',
+  },
+  {
+    id: 2,
+    image: car2,
+    alt: 'car mustang',
+    name: 'Mustang',
+    year: 2015,
+    speed: 180,
+    energyRating: 9,
+    userRating: 8,
+    model: 'car2',
+  },
+  {
+    id: 3,
+    image: car3,
+    alt: 'car mustang',
+    name: 'Mustang',
+    year: 2015,
+    speed: 180,
+    energyRating: 9,
+    userRating: 8,
+    model: 'car3',
+  },
+];
+
+export default data;
