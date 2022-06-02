c90eb2d76db46e3a35003aae1bc7ed03e0d321e0
Fernando Segregio <segregio@gmail.com>
2022-06-02 16:32:19 -0300

    feat: render texts section about according windon size

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import useWindowSize from '../../hooks/useWindowSize';
 import Carousel from '../Carousel';
 import {
   ArticleAbout,
@@ -9,6 +10,7 @@ import {
 } from './style';
 
 function Main() {
+  const size = useWindowSize();
   return (
     <MainContainer id="home">
       <SectionMain>
@@ -18,14 +20,31 @@ function Main() {
       <SectionAbout id="about">
         <div />
         <ArticleAbout>
-          <h1>O que são</h1>
-          <p>
-            A sigla SUV significa Sport Utility Vehicle -- ou seja,
-            veículo utilitário esportivo.
-            As SUVs costumam ter porte avantajado,
-            além de interior espaçoso e possibilidade de
-            trafegar dentro e fora da cidade.
-          </p>
+          {
+            size.width < 800 ? (
+              <>
+                <h1>O que são</h1>
+                <p>
+                  A sigla SUV significa Sport Utility Vehicle -- ou seja,
+                  veículo utilitário esportivo.
+                  As SUVs costumam ter porte avantajado,
+                  além de interior espaçoso e possibilidade de
+                  trafegar dentro e fora da cidade.
+                </p>
+              </>
+            ) : (
+              <>
+                <h1>Mustang</h1>
+                <p>
+                  {`O Ford Mustang é um automóvel desportivo produzido
+            pela Ford Motor Company. O carro foi apresentado ao
+            público em 17 de abril de 1964 durante a New York World's Fair.
+            O Mustang, apesar de ter sofrido grandes alterações
+            ao longo dos anos é a mais antiga linha de automóveis da Ford.`}
+                </p>
+              </>
+            )
+          }
           <ButtonAbout type="button">Ver carros</ButtonAbout>
         </ArticleAbout>
       </SectionAbout>
