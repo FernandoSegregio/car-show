7858368b2498c63ec563fd475f903380cb61b2e1
Fernando Segregio <segregio@gmail.com>
2022-06-03 02:39:04 -0300

    feat: add ThemeProvider from styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   BrowserRouter,
   Routes,
   Route,
 } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import HomePage from './pages/HomePage';
+import Table from './pages/Table';
+import { dark, light } from './styles/theme';
+import CarShowContext from './context/CarShowContext';
 
 function App() {
+  const { theme } = useContext(CarShowContext);
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <ThemeProvider theme={theme === 'dark' ? dark : light}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/table" element={<Table />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
