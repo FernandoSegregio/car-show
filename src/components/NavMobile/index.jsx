646947ee2c86d6239e44e60390b5f3ab522958a0
Fernando Segregio <segregio@gmail.com>
2022-06-02 22:09:59 -0300

    feat: add nav mobile with render conditional

---
+++ src/components/NavMobile/index.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import {
+  HeaderContainer,
+  NavHeader,
+  Hamburguer,
+  CloseNav,
+  UlNav,
+  Li,
+} from './style';
+
+function NavMobile() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  function openMenu() {
+    return isOpen ? setIsOpen(false) : setIsOpen(true);
+  }
+
+  return (
+    <HeaderContainer>
+      <Hamburguer onClick={() => openMenu()} type="button" hidden={!isOpen}>
+        <span className="iconify" data-icon="cil:hamburger-menu" />
+      </Hamburguer>
+      <CloseNav onClick={() => openMenu()} type="button" hidden={isOpen} />
+      <NavHeader hidden={isOpen}>
+        <span className="iconify" data-icon="carbon:logo-delicious" />
+        <UlNav>
+          <a href="#home" onClick={() => openMenu()}>
+            <Li>Home</Li>
+          </a>
+          <Li>Fale Conosco</Li>
+          <a href="#about" onClick={() => openMenu()}>
+            <Li>Sobre</Li>
+          </a>
+          <Li>Tabela</Li>
+        </UlNav>
+      </NavHeader>
+    </HeaderContainer>
+  );
+}
+
+export default NavMobile;
