980bd6c874d35f0f76afdd232d8aa3059ff073ad
Fernando Segregio <segregio@gmail.com>
2022-06-02 22:12:48 -0300

    feat: conditional render to nav mobile or nav desktop

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,41 +1,11 @@
-import React, { useState } from 'react';
-import {
-  HeaderContainer,
-  NavHeader,
-  Hamburguer,
-  CloseNav,
-  UlNav,
-  Li,
-} from './style';
+import React from 'react';
+import useWindowSize from '../../hooks/useWindowSize';
+import NavMobile from '../NavMobile';
+import NavDesktop from '../NavDesktop';
 
 function Header() {
-  const [isOpen, setIsOpen] = useState(true);
-
-  function openMenu() {
-    return isOpen ? setIsOpen(false) : setIsOpen(true);
-  }
-
-  return (
-    <HeaderContainer>
-      <Hamburguer onClick={() => openMenu()} type="button" hidden={!isOpen}>
-        <span className="iconify" data-icon="cil:hamburger-menu" />
-      </Hamburguer>
-      <CloseNav onClick={() => openMenu()} type="button" hidden={isOpen} />
-      <NavHeader hidden={isOpen}>
-        <span className="iconify" data-icon="carbon:logo-delicious" />
-        <UlNav>
-          <a href="#home" onClick={() => openMenu()}>
-            <Li>Home</Li>
-          </a>
-          <Li>Fale Conosco</Li>
-          <a href="#about" onClick={() => openMenu()}>
-            <Li>Sobre</Li>
-          </a>
-          <Li>Tabela</Li>
-        </UlNav>
-      </NavHeader>
-    </HeaderContainer>
-  );
+  const size = useWindowSize();
+  return size.width < 800 ? <NavMobile /> : <NavDesktop />;
 }
 
 export default Header;
