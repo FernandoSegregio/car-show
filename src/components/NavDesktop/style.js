e8b540c9b54b794c37d2838f3fc6070635fe1307
Fernando Segregio <segregio@gmail.com>
2022-06-02 22:10:18 -0300

    style: create nav desktop with render conditional

---
+++ src/components/NavDesktop/style.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  height: 50px;
+  background-color: transparent;
+  width: 100%;
+  display: flex;
+  position: absolute;
+
+  & > svg {
+      z-index: 2;
+      width: 65px;
+      height: 65px;
+      margin: 24.13px 0 0 32.12px;
+      color: var(--neutral-color-white-100);
+    }
+`;
+
+export const NavHeader = styled.nav`
+  width: calc(100% - 48px);
+  display: flex;
+  z-index: 2;
+  margin: 30px 60px 0;
+
+    a {
+      text-decoration: none;
+    }
+`;
+
+export const UlNav = styled.ul`
+  height: 50px;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Li = styled.li`
+  font-size: var(--font-size-m);
+  font-weight: var(--font-weight-500);
+  line-height: var(--line-height-m);
+  color: var(--neutral-color-white-100);
+  text-align: left;
+  list-style: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+
+    svg {
+      margin-left: 15.25px;
+      height: 40.5px;
+      width: 38.88px;
+    }
+`;
