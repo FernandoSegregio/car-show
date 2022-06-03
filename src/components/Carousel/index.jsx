45617c81a490f0d57cd2fab8ad9d44bacff4b8ac
Fernando Segregio <segregio@gmail.com>
2022-06-03 13:15:00 -0300

    fix: change version swiper

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,11 +1,10 @@
-/* eslint-disable import/no-unresolved */
 import React from 'react';
 import { SwiperSlide } from 'swiper/react';
 
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import 'swiper/css/bundle';
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+import 'swiper/components/pagination/pagination.min.css';
 
 import { Pagination, Navigation } from 'swiper';
 import {
