c6ecdb68b313c7db9e4240eb09bfcd3e90c6ce7f
Fernando Segregio <segregio@gmail.com>
2022-06-03 13:17:13 -0300

    test: verify h1 in component main

---
+++ src/components/Main/main.test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import renderWithRouter from '../../helpers/renderWithRouter';
+import Main from './index';
+
+describe('Testing component Main', () => {
+  it('should be able to show a h1 element.', () => {
+    renderWithRouter(<Main />);
+
+    const h1Element = screen.getAllByRole('heading', {
+      level: 1,
+    });
+    expect(h1Element[0]).toBeInTheDocument();
+    expect(h1Element[1]).toBeInTheDocument();
+    expect(h1Element).toHaveLength(2);
+  });
+});
