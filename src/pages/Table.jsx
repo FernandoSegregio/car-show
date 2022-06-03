4756052f08110e05cdab1c9fb3d44d4c3d18d544
Fernando Segregio <segregio@gmail.com>
2022-06-03 02:41:38 -0300

    feat: render components in page Table

---
+++ src/pages/Table.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TableList from '../components/TableList';
+import NavDesktop from '../components/NavDesktop';
+
+function Table() {
+  return (
+    <>
+      <NavDesktop />
+      <TableList />
+    </>
+  );
+}
+
+export default Table;
