41ae6d897bf706804e85a6e58ef5cb69871bcb42
Fernando Segregio <segregio@gmail.com>
2022-06-08 03:04:06 -0300

    feat: add logical with controller state and add news itens in data

--- src/components/TableList/index.jsx
+++ src/components/TableList/index.jsx
@@ -8,9 +8,50 @@ import CarShowContext from '../../context/CarShowContext';
 import retangle from '../../assets/images/addCar/retangle.png';
 
 function TableList() {
-  const [isHidden, setIsHidden] = useState('is-hidden');
   const location = useLocation();
+
+  const [isHidden, setIsHidden] = useState('is-hidden');
   const { setTheme } = useContext(CarShowContext);
+  const [nameCar, setNameCar] = useState('');
+  const [yearCar, setYearCar] = useState('');
+  const [speedCar, setSpeedCar] = useState('');
+  const [energyCar, setEnergyCar] = useState('');
+  const [userRatingCar, setUserRatingCar] = useState('');
+  const [modelCar, setModelCar] = useState('');
+  const [linkCar, setLinkCar] = useState('');
+
+  const newData = {
+    id: data[data.length - 1].id + 1,
+    alt: nameCar,
+    name: nameCar,
+    year: yearCar,
+    speed: speedCar,
+    energyRating: energyCar,
+    userRating: userRatingCar,
+    model: modelCar,
+    image: linkCar,
+  };
+
+  const nameAndYear = newData.name.length >= 2 && newData.year.length === 4;
+  const speedAndEnergy = newData.speed.length >= 2 && newData.energyRating.length >= 1;
+  const userRatingAndModelImage = newData.userRating.length >= 1 && newData
+    .model.length >= 2 && newData.image.length >= 2;
+
+  const desability = (nameAndYear && speedAndEnergy && userRatingAndModelImage);
+
+  function addNewCar() {
+    data.push(newData);
+  }
+
+  function resetInputs() {
+    setNameCar('');
+    setYearCar('');
+    setSpeedCar('');
+    setEnergyCar('');
+    setUserRatingCar('');
+    setModelCar('');
+    setLinkCar('');
+  }
 
   useEffect(() => {
     if (location.pathname.includes('table')) {
@@ -27,41 +68,58 @@ function TableList() {
         <h2>Adicionar novo</h2>
         <div>
           <img src={retangle} alt="bandeira" />
-          <Form>
+          <Form onSubmit={(e) => {
+            e.preventDefault();
+            addNewCar();
+          }}
+          >
             <label htmlFor="name">
               Nome
-              <input id="name" type="text" />
+              <input onChange={({ target: { value } }) => setNameCar(value)} id="name" type="text" name="name" value={nameCar} />
+            </label>
+            <label htmlFor="model">
+              Modelo
+              <input onChange={({ target: { value } }) => setModelCar(value)} id="model" type="text" name="model" value={modelCar} />
             </label>
             <label htmlFor="name">
               Ano
-              <input id="name" type="text" />
+              <input onChange={({ target: { value } }) => setYearCar(value)} id="year" type="text" name="year" value={yearCar} />
             </label>
-            <label htmlFor="name">
+            <label htmlFor="speed">
               Velocidade Máxima Km/h
-              <input id="name" type="text" />
+              <input onChange={({ target: { value } }) => setSpeedCar(value)} id="speed" type="text" name="speed" value={speedCar} />
             </label>
-            <label htmlFor="name">
+            <label htmlFor="energyRating">
               Nota economia
-              <input id="name" type="text" />
+              <input onChange={({ target: { value } }) => setEnergyCar(value)} id="energyRating" type="text" name="energyRating" value={energyCar} />
             </label>
-            <label htmlFor="name">
+            <label htmlFor="userRating">
               Nota usúarios
-              <input id="name" type="text" />
+              <input onChange={({ target: { value } }) => setUserRatingCar(value)} id="userRating" type="text" name="userRating" value={userRatingCar} />
             </label>
-            <label htmlFor="name">
+            <label htmlFor="link">
               Link produtos
-              <input id="name" type="text" />
+              <input onChange={({ target: { value } }) => setLinkCar(value)} id="link" type="text" name="link" value={linkCar} />
             </label>
+            <FormButton
+              disabled={!desability}
+              type="submit"
+              onClick={() => resetInputs()}
+            >
+              <span className="iconify" data-icon="carbon:add" />
+              Adicionar Novo
+            </FormButton>
           </Form>
         </div>
-        <FormButton>
-          <span className="iconify" data-icon="carbon:add" />
-          Adicionar Novo
-        </FormButton>
       </FormContainer>
       <article>
         <h1>Lista</h1>
-        <ButtonAdd type="button" onClick={() => setIsHidden('no-hidden')}>
+        <ButtonAdd
+          type="button"
+          onClick={() => {
+            setIsHidden('no-hidden');
+          }}
+        >
           <span className="iconify" data-icon="carbon:add" />
           Adicionar Novo
         </ButtonAdd>
@@ -79,7 +137,7 @@ function TableList() {
           </tr>
         </thead>
         <tbody>
-          { data.map(({
+          { data.length > 0 && data.map(({
             name, year, speed, energyRating, userRating, id,
           }) => (
             <tr key={id}>
